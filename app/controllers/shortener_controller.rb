1538997c658f1c4c967102f8d867dd37862d9af0
Matt Brace <mbrace@invodo.com>
2018-04-18 23:23:09 -0500

    Add documentation and update display text

--- app/controllers/shortener_controller.rb
+++ app/controllers/shortener_controller.rb
@@ -2,16 +2,22 @@ class ShortenerController < ApplicationController
   # Ignore security for post shorten calls, any call is good
   skip_before_action :verify_authenticity_token
 
+  # Default entrypoint, displays the main page
   def index
   end
 
+  # Shortens a URL that is provided in the request body
   def shorten
+    # Find any existing links for the provided URL so we don't generate duplicates
     prev_link = Link.find_by(link: params[:link])
+    # Generate a new link id if there isn't a previous link
     link_id = if prev_link
       prev_link.link_id
     else
       generate_id()
     end
+
+    # Generate the synthetic parameters for creating a new link
     p = ActionController::Parameters.new({
       link: {
         link: params[:link],
@@ -19,15 +25,21 @@ class ShortenerController < ApplicationController
       }
     })
 
+    # Create a new link if necessary
     link = if prev_link
       prev_link
     else
       Link.new(p.require(:link).permit(:link, :link_id))
     end
+
+    # Save the state of the link prior to rendering
     link.save()
+    # Render just the shortened URL back to the client
     render plain: "http://localhost:3000/" + link_id
   end
 
+  # Given a link id, find the corresponding link and perform
+  # a 302 redirect for it.
   def unshorten
     link = Link.find_by(link_id: params[:id])
     if link
@@ -35,17 +47,11 @@ class ShortenerController < ApplicationController
     end
   end
 
+  # Generate the link id to use
   def generate_id
     # Generate an 8 character string (0...8)
     # Made up of random uppper case letters
     id = (0...8).map { (65 + rand(26)).chr }.join
     return id
   end
-
-  private
-    def link_attributes
-      permitted = params.require :link
-      puts "Permitted: " + permitted
-      params.required(:link).permit(:link_id)#.required(:link_id)
-    end
 end
